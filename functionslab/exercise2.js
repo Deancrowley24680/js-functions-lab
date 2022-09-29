6f810169b7b169c5378be3478fe6357b53e8931f
Dean Crowley <deancrowley1@gmail.com>
2022-09-29 11:51:36 +0100

    Exercise 2

---
+++ functionslab/exercise2.js
@@ -0,0 +1,11 @@
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then(response => response.json())
+  .then(todo => {
+     const todoTitles = todo.map(
+        todo => todo.title);
+        console.log(todoTitles)
+  })
+  .catch(function(err) { 
+    console.log(err);
+  });
+
